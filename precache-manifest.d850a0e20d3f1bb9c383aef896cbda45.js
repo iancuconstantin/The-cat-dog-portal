d173003064985cbd5835f99234735cf0beca26b5
Bulat Constantin <bulatconstantin@192-168-0-100.rdsnet.ro>
2022-02-23 18:15:40 +0200

    Updates

---
+++ precache-manifest.d850a0e20d3f1bb9c383aef896cbda45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ff060a2dee206ead9d1268ea635fd7",
+    "url": "/The-cat-dog-portal/index.html"
+  },
+  {
+    "revision": "cda0ecdd04a0960cfe2c",
+    "url": "/The-cat-dog-portal/static/css/2.1a95cbfb.chunk.css"
+  },
+  {
+    "revision": "2e15657aeea135295618",
+    "url": "/The-cat-dog-portal/static/css/main.73039ae1.chunk.css"
+  },
+  {
+    "revision": "cda0ecdd04a0960cfe2c",
+    "url": "/The-cat-dog-portal/static/js/2.796d83ef.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/The-cat-dog-portal/static/js/2.796d83ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e15657aeea135295618",
+    "url": "/The-cat-dog-portal/static/js/main.f65a6527.chunk.js"
+  },
+  {
+    "revision": "ae2173e7420c4124ced4",
+    "url": "/The-cat-dog-portal/static/js/runtime-main.2f60738f.js"
+  }
+]);
